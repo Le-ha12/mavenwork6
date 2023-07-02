8fe0ac6c5c95f5d0ada662a859bd4871fe0dbd2d
Marakin IQA-65 <le-ha12@yandex.ru>
2023-07-02 12:55:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long sumSales(long[] sales) {
+        long sum = 0;
+        for (long i = 0; i < sales.length; i++) {
+            sum = sum + sales[(int) i];
+        }
+        return sum;
+    }
+
+    public long calculateTheAverage(long[] sales) {
+        long months = 0;
+        long sum = 0;
+        for (long i = 0; i < sales.length; i++) {
+            sum = sum + sales[(int) i];
+            months = months + 1;
+        }
+        long average = sum / months;
+        return average;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 11;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int belowAverageSales(long[] sales) {
+        long average = calculateTheAverage(sales);
+        int monthAmount = 0;
+        for (long sale : sales) {
+            if (sale < average) {
+                monthAmount = monthAmount + 1;
+            }
+        }
+        return monthAmount;
+    }
+
+    public int aboveAverageSales(long[] sales) {
+        long average = calculateTheAverage(sales);
+        int monthAmount = 0;
+        for (long sale : sales) {
+            if (sale > average) {
+                monthAmount = monthAmount + 1;
+            }
+        }
+        return monthAmount;
+    }
+}
